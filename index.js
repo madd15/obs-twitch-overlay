498a216201237c4bf393b2b952e23a3094bdb64c
Matthew Taylor <matt@numenta.org>
2019-03-23 08:45:08 -0700

    webhook / socket cleanup

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-const deployed = process.env.DEPLOYED === 'true'
-const port = process.env.PORT || 8081
-let baseUrl = 'http://localhost'
-if (deployed) baseUrl = 'https://obs-twitch-overlay.herokuapp.com'
-let callbackUrl = baseUrl + '/_twitch_webhooks'
-
+let http = require('http');
 let express = require('express')
 let request = require('request')
+let io = require('socket.io');
 
+const port = process.env.PORT || 8081
+
+// Set up the URLS for this server.
+let baseUrl = 'http://localhost'
+if (process.env.HEROKU_URL) {
+    baseUrl = process.env.HEROKU_URL
+}
+let callbackUrl = baseUrl + '/_twitch_webhooks'
+
+// Create server instances
 const app = express()
-let http = require('http').Server(app);
-let socket = require('socket.io')(http);
+let server = http.Server(app);
+let socket = io(server);
 
+// Global vars for server.
 let clientId = 'fmjgn1bqxpw7p0xgvryoe6027483ve'
-// let clientSecret = process.env.APP_SECRET
-let clientSecret = 's1u6yj9aexfmn7pp0vvgeiwfktbmar'
+let clientSecret = process.env.CLIENT_SECRET || 's1u6yj9aexfmn7pp0vvgeiwfktbmar'
 let streamId = 'firstinspires1'
+// This will be set on server startup.
 let appAccessToken
-
-
 
 // This serves the static files for the JS client program.
 app.use(express.static('static'))
 
+// This is the Twitch API proxy used by the client to make Twitch API calls.
+// TODO: Remove this in favor of all API calls originating fromt he server.
 app.get('/_twitch', (req, res) => {
     let query = req.query
     const turl = query.qs.turl
@@ -48,7 +55,8 @@ app.get('/_twitch', (req, res) => {
     })
 })
 
-// For Twitch webhooks
+// This is the URL that Twitch will call with webhook subscription updates.
+// We are simply broadcasting them to the client.
 app.get('/_twitch_webhooks', (req, res) => {
     console.warn('webhook received!')
     let q = req.query
@@ -58,12 +66,13 @@ app.get('/_twitch_webhooks', (req, res) => {
         console.log('returning hub.challenge')
         res.status(200).end(code)
     } else {
-        console.log('UNKNOWN WEBHOOK PACKAGE:')
+        console.log('Passing webhook payload to socket client.')
         socket.emit('webhook', q);
         res.end()
     }
 })
 
+// Following the Twitch API authentication flow by getting an app access token.
 function authenticate(cb) {
     if (! appAccessToken) {
         let opts = {
@@ -89,25 +98,23 @@ function authenticate(cb) {
     }
 }
 
-
+// Gets rid of any existing webhooks.
 function clearExistingSubs(cb) {
-    authenticate(() => {
-        let payload = {
-            url: 'https://api.twitch.tv/helix/webhooks/subscriptions',
-            headers: {
-                'Client-ID': clientId,
-                'Authorization': `Bearer ${appAccessToken}`,
-            }
+    let payload = {
+        url: 'https://api.twitch.tv/helix/webhooks/subscriptions',
+        headers: {
+            'Client-ID': clientId,
+            'Authorization': `Bearer ${appAccessToken}`,
         }
-        console.log('searching for existing webhooks...')
-        request(payload, (error, resp, rawBody) => {
-            let hooks = JSON.parse(rawBody).data
-            // can delete these asynchronously
-            hooks.forEach(hook => {
-                webhookSubscribe('unsubscribe', hook.topic)
-            })
-            cb()
+    }
+    console.log('searching for existing webhooks...')
+    request(payload, (error, resp, rawBody) => {
+        let hooks = JSON.parse(rawBody).data
+        // can delete these asynchronously
+        hooks.forEach(hook => {
+            webhookSubscribe('unsubscribe', hook.topic)
         })
+        cb()
     })
 }
 
@@ -143,7 +150,7 @@ function webhookSubscribe(mode, topic) {
         },
     }, function (error, response, body) {
         if (response && response.statusCode === 202) {
-            console.log('Webhook subscription accepted... awaiting creation.')
+            console.log('Webhook subscription change accepted.')
         } else {
             console.log(error)
         }
@@ -160,40 +167,26 @@ function updateWebhookSubscriptions(login) {
     })
 }
 
-updateWebhookSubscriptions(streamId)
+function setupSocketComms() {
+    server.listen(port, function(){
+        console.log(`serving HTML from ${baseUrl}:${port}/index.html`)
+        console.log(`listening on *:${port}`);
+    });
 
-http.listen(port, function(){
-    console.log(`serving HTML from ${baseUrl}:${port}/index.html`)
-    console.log(`listening on *:${port}`);
-});
+    socket.on('connect', function(){
+        console.log('connect cb')
+    });
+    socket.on('event', function(data){
+        console.log('event cb')
+        console.log(data)
+    });
+    socket.on('disconnect', function(){
+        console.log('disconnect cb')
+    });
+}
 
-socket.on('connect', function(){
-    console.log('connect cb')
-});
-socket.on('event', function(data){
-    console.log('event cb')
-    console.log(data)
-});
-socket.on('disconnect', function(){
-    console.log('disconnect cb')
-});
-
-
-// twitch.getGame('Science & Technology', (err, scienceAndTech) => {
-//
-//     let updateLiveStreams = () => {
-//         console.log('')
-//         twitch.getStreams(scienceAndTech.data[0].id, (streamError, streams) => {
-//             liveStreams = streams.data
-//             console.table(
-//                 liveStreams,
-//                 ['viewer_count', 'user_name', 'started_at', 'title']
-//             )
-//         })
-//     }
-//
-//     updateLiveStreams()
-//
-//     setInterval(updateLiveStreams, 10000)
-// })
-
+// Program start
+setupSocketComms()
+authenticate(() => {
+    updateWebhookSubscriptions(streamId)
+})
