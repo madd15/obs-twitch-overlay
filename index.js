755b6f917ad20899fc017109ee0ba2c81eca75ba
Matthew Taylor <matt@numenta.org>
2019-03-23 20:09:27 -0700

    Testing webhooks live

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 let http = require('http');
 let express = require('express')
-let request = require('request')
 let io = require('socket.io')
+
+// I wrote these classes
 let Webhooks = require('./src/webhooks')
 let Twitch = require('./src/twitch')
 
-const port = process.env.PORT || 8081
-
-// Set up the URLS for this server.
+// Server settings.
 let baseUrl = 'http://localhost'
 if (process.env.HEROKU_URL) {
     baseUrl = process.env.HEROKU_URL
 }
-let callbackUrl = baseUrl + '/_twitch_webhooks'
+let hookUrl = baseUrl + '/_twitch_webhooks'
+const port = process.env.PORT || 8081
 
-// Create server instances
+// Create server and comms.
 const app = express()
 let server = http.Server(app);
 let socket = io(server);
@@ -22,43 +22,16 @@ let socket = io(server);
 // Global vars for server.
 let clientId = 'fmjgn1bqxpw7p0xgvryoe6027483ve'
 let clientSecret = process.env.CLIENT_SECRET || 's1u6yj9aexfmn7pp0vvgeiwfktbmar'
-let targetLogin = 'jitspoe'
+
+// The Twitch user to monitor
+let targetLogin = 'rhyolight_'
 
 // This serves the static files for the JS client program.
 app.use(express.static('static'))
 
-// // This is the Twitch API proxy used by the client to make Twitch API calls.
-// // TODO: Remove this in favor of all API calls originating fromt he server.
-// app.get('/_twitch', (req, res) => {
-//     let query = req.query
-//     const turl = query.qs.turl
-//     delete query.qs.turl
-//     const options = {
-//         headers: {
-//             'Client-ID': clientId,
-//             'User-Agent': 'request',
-//         }
-//     }
-//     let opts = Object.assign({}, options, {
-//         url: turl,
-//         qs: query.qs,
-//     })
-//     console.log(`Proxy HTTP call to ${turl}...`)
-//     request(opts, (error, resp, rawBody) => {
-//         let body = JSON.parse(rawBody)
-//         if (resp.statusCode !== 200) {
-//             console.log('%s: %s', body.error, body.message)
-//             process.exit()
-//         }
-//         res.setHeader('Content-Type', 'application/json');
-//         res.end(rawBody);
-//     })
-// })
-
 // This is the URL that Twitch will call with webhook subscription updates.
-// We are simply broadcasting them to the client.
-app.get('/_twitch_webhooks', (req, res) => {
-    console.warn('webhook received!')
+app.post('/_twitch_webhooks', (req, res) => {
+    console.log('webhook received!')
     let q = req.query
     // this could be a subscription challenge
     if (q['hub.mode']) {
@@ -69,6 +42,8 @@ app.get('/_twitch_webhooks', (req, res) => {
         console.log('Passing webhook payload to socket client.')
         // TODO: parse payload and emit them over socket
         // socket.emit('<event-type>', q);
+        console.log(q)
+        socket.emit('stream', q.data[0])
         res.end()
     }
 })
@@ -96,7 +71,7 @@ startServer(twitch, targetLogin)
 twitch.authenticate((token) => {
     if (! token) throw new Error('Cannot get app token')
     twitch.getUser(targetLogin, (user) => {
-        let webhooks = new Webhooks(user, callbackUrl, token)
+        let webhooks = new Webhooks(user, hookUrl, token)
         webhooks.updateWebhookSubscriptions(user, token)
     })
 })
