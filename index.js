114c0b5fe46bdcbfe3bebe972e924d837ae853a8
Matthew Taylor <matt@numenta.org>
2019-03-23 20:00:22 -0700

    a lot of refactoring

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let express = require('express')
 let request = require('request')
 let io = require('socket.io')
 let Webhooks = require('./src/webhooks')
+let Twitch = require('./src/twitch')
 
 const port = process.env.PORT || 8081
 
@@ -21,38 +22,38 @@ let socket = io(server);
 // Global vars for server.
 let clientId = 'fmjgn1bqxpw7p0xgvryoe6027483ve'
 let clientSecret = process.env.CLIENT_SECRET || 's1u6yj9aexfmn7pp0vvgeiwfktbmar'
-let streamId = 'rhyolight_'
+let targetLogin = 'jitspoe'
 
 // This serves the static files for the JS client program.
 app.use(express.static('static'))
 
-// This is the Twitch API proxy used by the client to make Twitch API calls.
-// TODO: Remove this in favor of all API calls originating fromt he server.
-app.get('/_twitch', (req, res) => {
-    let query = req.query
-    const turl = query.qs.turl
-    delete query.qs.turl
-    const options = {
-        headers: {
-            'Client-ID': clientId,
-            'User-Agent': 'request',
-        }
-    }
-    let opts = Object.assign({}, options, {
-        url: turl,
-        qs: query.qs,
-    })
-    console.log(`Proxy HTTP call to ${turl}...`)
-    request(opts, (error, resp, rawBody) => {
-        let body = JSON.parse(rawBody)
-        if (resp.statusCode !== 200) {
-            console.log('%s: %s', body.error, body.message)
-            process.exit()
-        }
-        res.setHeader('Content-Type', 'application/json');
-        res.end(rawBody);
-    })
-})
+// // This is the Twitch API proxy used by the client to make Twitch API calls.
+// // TODO: Remove this in favor of all API calls originating fromt he server.
+// app.get('/_twitch', (req, res) => {
+//     let query = req.query
+//     const turl = query.qs.turl
+//     delete query.qs.turl
+//     const options = {
+//         headers: {
+//             'Client-ID': clientId,
+//             'User-Agent': 'request',
+//         }
+//     }
+//     let opts = Object.assign({}, options, {
+//         url: turl,
+//         qs: query.qs,
+//     })
+//     console.log(`Proxy HTTP call to ${turl}...`)
+//     request(opts, (error, resp, rawBody) => {
+//         let body = JSON.parse(rawBody)
+//         if (resp.statusCode !== 200) {
+//             console.log('%s: %s', body.error, body.message)
+//             process.exit()
+//         }
+//         res.setHeader('Content-Type', 'application/json');
+//         res.end(rawBody);
+//     })
+// })
 
 // This is the URL that Twitch will call with webhook subscription updates.
 // We are simply broadcasting them to the client.
@@ -66,54 +67,23 @@ app.get('/_twitch_webhooks', (req, res) => {
         res.status(200).end(code)
     } else {
         console.log('Passing webhook payload to socket client.')
-        socket.emit('webhook', q);
+        // TODO: parse payload and emit them over socket
+        // socket.emit('<event-type>', q);
         res.end()
     }
 })
 
-// Following the Twitch API authentication flow by getting an app access token.
-function authenticate(cb) {
-    let opts = {
-        url: 'https://www.reddit.com/r/funny.json',
-        headers: {
-            'Client-ID': clientId,
-            'Authorization': `Bearer ${clientSecret}`,
-        },
-        json: {
-            client_id: clientId,
-            client_secret: clientSecret,
-            grant_type: 'client_credentials',
-            scope:'',
-        }
-    };
-    request.post('https://id.twitch.tv/oauth2/token', opts, (error, resp, body) => {
-        console.log('auth returned... storing access token')
-        cb(body.access_token)
-    })
-}
 
-function getUser(login, cb) {
-    request('https://api.twitch.tv/helix/users', {
-        headers: {
-            'Client-ID': clientId,
-            'User-Agent': 'request',
-        },
-        qs: {
-            login: login,
-        }
-    }, (error, resp, rawBody) => {
-        cb(JSON.parse(rawBody).data[0])
-    })
-
-}
-
-function startServer() {
+function startServer(twitch, login) {
     server.listen(port, function(){
         console.log(`serving HTML from ${baseUrl}:${port}/index.html`)
         console.log(`listening on *:${port}`);
     });
     socket.on('connect', function(){
         console.log('Connected to socket client.')
+        twitch.getStream(login, (stream) => {
+            socket.emit('stream', stream)
+        })
     });
     socket.on('disconnect', function(){
         console.log('Disconnected from socket client.')
@@ -121,10 +91,11 @@ function startServer() {
 }
 
 // Program start
-startServer()
-authenticate((token) => {
+let twitch = new Twitch(clientId, clientSecret)
+startServer(twitch, targetLogin)
+twitch.authenticate((token) => {
     if (! token) throw new Error('Cannot get app token')
-    getUser(streamId, (user) => {
+    twitch.getUser(targetLogin, (user) => {
         let webhooks = new Webhooks(user, callbackUrl, token)
         webhooks.updateWebhookSubscriptions(user, token)
     })
